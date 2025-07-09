410cd1bd7de286f64cd6b32ec3cdf98488833ef1
naymilikeschoco <153507473+naymilikeschoco@users.noreply.github.com>
2025-07-09 15:11:10 +0800

    Add files via upload

---
+++ my_NPC.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+module my_NPC(
+    input  wire [31:0]  offset,
+    input  wire         br,
+    input  wire [1:0]   npc_op,
+    input  wire [31:0]  pc,
+    output reg [31:0]   pcb, //”√”⁄–¥ªÿ
+    output reg [31:0]   npc
+    );
+    
+    parameter NPC_PC4 = 0;
+    parameter NPC_BRC = 1;
+    parameter NPC_JMP = 2;
+    parameter NPC_PC4_ADD = 3;
+
+    always @ (*) begin
+        if(npc_op == NPC_PC4)begin
+            npc <= pc + 4;
+            pcb <= pc + 4; //not used
+        end
+        else if(npc_op == NPC_BRC)begin
+            if(br) npc <= pc + offset;
+            else npc <= pc + 4;
+            pcb <= pc + 4; //not used
+        end
+        else if(npc_op == NPC_JMP)begin
+            npc <= pc + offset;
+            pcb <= pc + 4;
+        end
+        else begin //npc_op == NPC_PC4_ADD (for pcaddu12i)
+            npc <= pc + 4;
+            pcb <= pc + offset;
+        end
+    end
+
+endmodule
---
+++ my_RF.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module my_RF(
+    input  wire         clk,
+    input  wire [4:0]   rR1, //[14:10]
+    input  wire [4:0]   rR2, //[9:5]
+    input  wire [4:0]   wR,  //[4:0]
+    input  wire         rd1_op,
+    input  wire         we,
+    input  wire [31:0]  wD,
+    output wire [31:0]  rD1,
+    output wire [31:0]  rD2
+    );
+    
+    parameter RD1_3R = 0;
+    parameter RD1_2R = 1; 
+    
+    reg [31:0] Regs[4:0];
+    //Ð´Ê±ÐòÂß¼­
+    always @ (posedge clk) begin
+        Regs[0] <= 0; //$zero always == 0
+        if(we) begin //Ð´Ê¹ÄÜ
+            Regs[wR] <= wD;
+        end
+    end
+    
+    //¶Á×éºÏÂß¼­
+    assign rD1 = (rd1_op == RD1_3R)? Regs[rR1]: Regs[wR];
+    assign rD2 = Regs[rR2];
+    
+endmodule
