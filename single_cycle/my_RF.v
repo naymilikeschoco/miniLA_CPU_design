7c5385eec1d53c874a132925b4ebe2d736d0de0b
naymilikeschoco <153507473+naymilikeschoco@users.noreply.github.com>
2025-07-18 14:05:43 +0800

    Update my_RF.v
    修改了bl指令遗漏指定写回寄存器r1的情况

--- single_cycle/my_RF.v
+++ single_cycle/my_RF.v
@@ -7,7 +7,7 @@ module my_RF(
     input  wire [4:0]    rR1,  // [14:10]
     input  wire [4:0]    rR2,  // [9:5]
     input  wire [4:0]    wR,   // [4:0]
-    input  wire          rd1_op,
+    input  wire [1:0]    rf_op,
     input  wire          we,
     input  wire [31:0]   wD,
     output wire  [31:0]  rD1,
@@ -17,12 +17,13 @@ module my_RF(
     reg [31:0] Regs[31:0];
 
     always @(posedge clk) begin
-        if (we && (wR != 5'b00000)) begin
-            Regs[wR] <= wD;
+        if (we) begin
+            if(rf_op == `WR_1)  Regs[1] <= wD;
+            else if(wR != 5'b00000)  Regs[wR] <= wD;
         end
     end
     
-    assign rD1 = ((rR1 | wR) == 5'b0) ? 32'b0 : ((rd1_op == `RD1_3R)? Regs[rR1] : Regs[wR]);
+    assign rD1 = ((rR1 | wR) == 5'b0) ? 32'b0 : ((rf_op == `RD1_3R)? Regs[rR1] : Regs[wR]);
     assign rD2 = (rR2 == 5'b0) ? 32'b0 : Regs[rR2];
 
     
